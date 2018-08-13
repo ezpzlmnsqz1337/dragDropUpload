5348fda3499b0a322fd09747e066bf560dc45e6a
Zdeněk Braun <zdenek.braun@siemens.com>
2018-08-13 08:49:06 +0200

    Working version with dropzone.js and no checks

--- php/upload.php
+++ php/upload.php
@@ -1,28 +1,33 @@
 <?php 
-      if(isset($_FILES['files'])){
-            $errors= array();
-            $file_name = $_FILES['files']['name'];
-            $file_size =$_FILES['files']['size'];
-            $file_tmp =$_FILES['files']['tmp_name'];
-            $file_type=$_FILES['files']['type'];
-            $file_ext=strtolower(end(explode('.',$_FILES['files']['name'])));
-            
-            /*$expensions= array("jpeg","jpg","png","md");
-            
-            if(in_array($file_ext,$expensions)=== false){
-                  $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-            }
-            
-            if($file_size > 2097152){
-                  $errors[]='File size must be excately 2 MB';
-            }*/
-            
-            if(empty($errors)==true){
-                  move_uploaded_file($file_tmp,"files/".$file_name);
-                  echo "Success";
-            }else{
-                  print_r($errors);
-                  echo "Failed";
-            }
-   }
+	if(isset($_FILES['file'])){
+		print_r($_FILES);
+	    $errors= array();
+
+		$root_dir = dirname(__FILE__);
+
+    	$file_name = $_FILES['file']['name'];
+        $file_size = $_FILES['file']['size'];
+        $file_tmp = $_FILES['file']['tmp_name'];
+        $file_type = $_FILES['file']['type'];
+        $file_ext = strtolower(end(explode('.',$_FILES['file']['name'])));
+
+	    
+        if(empty($errors)==true){
+              echo move_uploaded_file($file_tmp,$root_dir."/files/".$file_name);
+              echo "Success";
+        }else{
+              print_r($errors);
+              echo "Failed";
+        }
+	    
+	    /*$expensions= array("jpeg","jpg","png","md");
+	    
+	    if(in_array($file_ext,$expensions)=== false){
+	          $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+	    }
+	    
+	    if($file_size > 2097152){
+	          $errors[]='File size must be excately 2 MB';
+	    }*/
+	}
 ?>
